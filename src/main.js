83ed9ea11667daf308b830a3543c920d92367a71
huangsir3 <287376183@qq.com>
2020-11-09 11:26:51 +0800

    搜素条件的对齐优化

--- src/main.js
+++ src/main.js
@@ -2,11 +2,25 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import * as filters from "./filters/filters";
+import ElementUI from "element-ui";
+import Print from "vue-print-nb";
+import "element-ui/lib/theme-chalk/index.css";
+import "@/assets/fonts/iconfont.css";
+import "@/assets/style/common.scss";
 
 Vue.config.productionTip = false;
+Vue.use(ElementUI);
+Vue.use(Print);
+
+// 注册全局过滤器
+Object.keys(filters).forEach(k => {
+    Vue.filter(k, filters[k]);
+    Vue.prototype[k] = filters[k];
+});
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,137 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
+const originalPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+    return originalPush.call(this, location).catch(err => err);
+};
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    {
+        path: "/",
+        redirect: { path: "/login" }
+    },
+    {
+        path: "/login",
+        component: () => import("../views/login/Login.vue")
+    },
+    {
+        path: "/home",
+        component: () => import("../views/home/Home.vue")
+    },
+    {
+        // 采集系统
+        path: "/collect",
+        redirect: {
+            path: "/collect/sampling"
+        },
+        component: {
+            template: `<router-view></router-view>`
+        },
+        children: [
+            {
+                // 采样数据
+                path: "sampling",
+                component: () => import("../views/collect/Sampling.vue")
+            },
+            {
+                // 检测数据
+                path: "check",
+                component: () => import("../views/collect/Check.vue")
+            }
+        ]
+    },
+    {
+        // 检测系统
+        path: "/check",
+        redirect: {
+            path: "/check/sampling"
+        },
+        component: {
+            template: `<router-view></router-view>`
+        },
+        children: [
+            {
+                // 采样数据
+                path: "sampling",
+                component: () => import("../views/check/Sampling.vue")
+            },
+            {
+                // 检测数据
+                path: "check",
+                component: () => import("../views/check/Check.vue")
+            }
+        ]
+    },
+    {
+        // 审批系统
+        path: "/approve",
+        redirect: {
+            path: "/approve/approveSpecimen"
+        },
+        component: {
+            template: `<router-view></router-view>`
+        },
+        children: [
+            {
+                // 审批样本
+                path: "approveSpecimen",
+                component: () => import("../views/approve/ApproveSpecimen.vue")
+            }
+        ]
+    },
+    {
+        // 管理系统
+        path: "/manage",
+        redirect: {
+            path: "/manage/user"
+        },
+        component: {
+            template: `<router-view></router-view>`
+        },
+        children: [
+            {
+                // 用户管理
+                path: "user",
+                component: () => import("../views/manage/User.vue")
+            },
+            {
+                // 用户管理
+                path: "role",
+                component: () => import("../views/manage/Role.vue")
+            },
+            {
+                // 采样点管理
+                path: "samplingPoint",
+                component: () => import("../views/manage/SamplingPoint.vue")
+            },
+            {
+                // 实验室管理
+                path: "laboratory",
+                component: () => import("../views/manage/Laboratory.vue")
+            },
+            {
+                // 数据字典管理
+                path: "dict",
+                component: () => import("../views/manage/Dict.vue")
+            },
+            {
+                // 报表管理
+                path: "report",
+                component: () => import("../views/manage/Report.vue")
+            },
+            {
+                // 操作日志
+                path: "log",
+                component: () => import("../views/manage/Log.vue")
+            }
+        ]
+    }
 ];
 
 const router = new VueRouter({
-  routes
+    routes
 });
 
 export default router;
